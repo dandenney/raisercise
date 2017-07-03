871a2bdb7cdce4fea7d5a53901d7b80794615269
dandenney <dd.subscriptions@gmail.com>
2017-07-02 22:51:56 -0400

    feat: Console log an exercise object from the form

--- gulpfile.js
+++ gulpfile.js
@@ -26,33 +26,38 @@
 //   Dependencies
 // -------------------------------------
 
-var gulp          = require('gulp');
-var sass          = require('gulp-sass');
-var autoprefixer  = require('gulp-autoprefixer');
-var browserSync   = require('browser-sync').create();
-var babel 				= require("gulp-babel");
-var concat        = require('gulp-concat');
-var uglify        = require('gulp-uglify');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var browserSync = require("browser-sync").create();
+var babel = require("gulp-babel");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
 
 // -------------------------------------
 //   Primary Task
 // -------------------------------------
 
-gulp.task('default', ['copy-html', 'copy-images', 'styles', 'scripts'], function() {
-	gulp.watch('src/styles/**/*.scss', ['styles']);
-	gulp.watch('src/js/**/*.js', ['scripts']);
-	gulp.watch('src/*.html', ['copy-html']);
-	gulp.watch('src/*.html').on('change', browserSync.reload);
+gulp.task(
+  "default",
+  ["copy-html", "copy-images", "styles", "scripts"],
+  function() {
+    gulp.watch("src/styles/**/*.scss", ["styles"]);
+    gulp.watch("src/js/**/*.js", ["scripts"]);
+    gulp.watch("src/*.html", ["copy-html"]);
+    gulp.watch("src/*.html").on("change", browserSync.reload);
 
-	gulp.src("src/js/**/*.js")
-    .pipe(babel())
-    .pipe(concat("all.js"))
-    .pipe(gulp.dest("dist"));
+    gulp
+      .src("src/js/**/*.js")
+      .pipe(babel())
+      .pipe(concat("all.js"))
+      .pipe(gulp.dest("dist"));
 
-	browserSync.init({
-		server: './dist'
-	});
-});
+    browserSync.init({
+      server: "./dist"
+    });
+  }
+);
 
 // -------------------------------------
 //   Individual Tasks
@@ -60,54 +65,59 @@ gulp.task('default', ['copy-html', 'copy-images', 'styles', 'scripts'], function
 
 // ----- Dist ----- //
 
-gulp.task('dist', [
-	'copy-html',
-	'copy-images',
-	'styles',
-	'scripts',
-	'scripts-dist'
+gulp.task("dist", [
+  "copy-html",
+  "copy-images",
+  "styles",
+  "scripts",
+  "scripts-dist"
 ]);
 
 // ----- JavaScript ----- //
 
-gulp.task('scripts', function() {
-	gulp.src('src/js/**/*.js')
-		.pipe(concat('app.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('dist/js'));
+gulp.task("scripts", function() {
+  gulp
+    .src("src/js/**/*.js")
+    .pipe(concat("app.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("dist/js"));
 });
 
-gulp.task('scripts-dist', function() {
-	gulp.src('src/js/**/*.js')
-		.pipe(concat('app.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('dist/js'));
+gulp.task("scripts-dist", function() {
+  gulp
+    .src("src/js/**/*.js")
+    .pipe(concat("app.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("dist/js"));
 });
 
 // ----- styles ----- //
 
-gulp.task('styles', function() {
-	gulp.src('src/styles/**/*.scss')
-		.pipe(sass({
-			outputStyle: 'compressed'
-		}).on('error', sass.logError))
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions']
-		}))
-		.pipe(gulp.dest('dist/styles'))
-		.pipe(browserSync.stream());
+gulp.task("styles", function() {
+  gulp
+    .src("src/styles/**/*.scss")
+    .pipe(
+      sass({
+        outputStyle: "compressed"
+      }).on("error", sass.logError)
+    )
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"]
+      })
+    )
+    .pipe(gulp.dest("dist/styles"))
+    .pipe(browserSync.stream());
 });
 
 // ----- HTML ----- //
 
-gulp.task('copy-html', function() {
-	gulp.src('src/*.html')
-		.pipe(gulp.dest('./dist'));
+gulp.task("copy-html", function() {
+  gulp.src("src/*.html").pipe(gulp.dest("./dist"));
 });
 
 // ----- Images ----- //
 
-gulp.task('copy-images', function() {
-	gulp.src('src/img/*')
-		.pipe(gulp.dest('dist/img'));
+gulp.task("copy-images", function() {
+  gulp.src("src/img/*").pipe(gulp.dest("dist/img"));
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,30 +1,30 @@
-// *************************************
-//
-//   File Name
-//   -> Description
-//
-// *************************************
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD1UCpu5L54wqZOn89UE5jAGlBWYgKoYXM",
+  authDomain: "progress-tracker-13d1c.firebaseapp.com",
+  databaseURL: "https://progress-tracker-13d1c.firebaseio.com",
+  projectId: "progress-tracker-13d1c",
+  storageBucket: "progress-tracker-13d1c.appspot.com",
+  messagingSenderId: "742939111596"
+};
+firebase.initializeApp(config);
 
-  // -------------------------------------
-  //   Private Variables
-  // -------------------------------------
+// Add an exercise to Firebase
+function addExercise() {
+  const exercisesRef = firebase.database().ref("exercises");
+  const exercise = {
+    title: document.querySelector("#newTitle").value,
+    setting: document.querySelector("#newSetting").value,
+    settingType: document.querySelector("#newSettingType").value,
+    reps: document.querySelector("#newReps").value,
+    raiseAfter: document.querySelector("#newRaiseAfter").value,
+    raiseBy: document.querySelector("#newRaiseBy").value
+  };
+  console.log(exercise);
+}
 
-  // ...
-
-  // -------------------------------------
-  //   Initialize
-  // -------------------------------------
-
-
-
-  // -------------------------------------
-  //   Set Event Handlers
-  // -------------------------------------
-
-
-// -------------------------------------
-//   Usage
-// -------------------------------------
-
-console.clear();
-console.log("🍻");
+// Event Listners
+document.querySelector("#addExercise").addEventListener("submit", function(e) {
+  e.preventDefault();
+  addExercise();
+});
